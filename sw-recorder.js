82f3b7721066d8b191e80a375d19aafa934abe96
Yukimi Kazari <dev@9854.moe>
2022-03-05 23:07:37 -0500

    Adding digital recording functionality - Adding M3U creation and ffmpeg recording

--- sw-recorder.js
+++ sw-recorder.js
@@ -28,137 +28,136 @@ let activeRecordings = new Map();
 let LastSyncChannels = new Map();
 
 console.log(`Lizumi Digital Recorder v0.1`);
-(async () => {
-    function newRadioStream(channelNumber) {
-        return new Promise(resolve => {
-            try {
-                request.get({
-                    url: `http://${config.sxmclient_host}/${channelNumber}.m3u8`,
-                }, async function (err, res, body) {
-                    if (err) {
-                        console.error("Faulty Server Response: " + err.message);
-                        resolve(false);
-                    } else {
-                        try {
-                            const data = body.toString();
-                            const _data = data.split('\n').map(e => {
-                                if (e.startsWith('#EXT-X-KEY')) {
-                                    return e.replace('URI="', `URI="http://${config.sxmclient_host}/`);
-                                } else if (e.startsWith('AAC_Data')) {
-                                    return `http://${config.sxmclient_host}/${e}`
-                                } else {
-                                    return e
-                                }
-                            })
-                            const startSync = moment(data.find(e => e.startsWith('#EXT-X-PROGRAM-DATE-TIME')).split('PROGRAM-DATE-TIME:').pop().split('+')[0]).valueOf();
-                            const stopSync = moment(data.reverse().find(e => e.startsWith('#EXT-X-PROGRAM-DATE-TIME')).split('PROGRAM-DATE-TIME:').pop().split('+')[0]).valueOf();
-                            resolve({
-                                playlist: _data,
-                                startSync,
-                                stopSync
-                            })
-                        } catch (e) {
-                            console.error('AAC Data Failure');
-                            console.error(e);
-                            resolve(false);
-                        }
-                    }
-                })
-            } catch (e) {
-                console.error(`Failed to get stream URLs!`)
-                console.error(e)
-                resolve(false);
-            }
-        })
-    }
-    function writeStreamSheet(playlist) {
-        return new Promise(resolve => {
-            let token = crypto.randomBytes(64).toString("hex");
-            fs.writeFile(path.join(config.record_dir, `.AACSTREAM_${token}.m3u8`), playlist.toString(), () => resolve(`.AACSTREAM_${token}.m3u8`))
-        })
-    }
-    function startNewRecording(channel, limiter) {
-        const maxFileTime = 2;
-        limiter.removeTokens(1, async () => {
-            const lastSync = (LastSyncChannels.has(channel)) ? LastSyncChannels.get(channel) : undefined;
-            const streamData = await newRadioStream(channel);
-            const steamFile = await writeStreamSheet(streamData.playlist.join('\n'));
-
-            const startTime = ((x,y) => {
-                if (lastSync) {
-                    if (x <= y && (x - y) > 0)
-                        return [[ '-ss', msToTime(x - y) ], y]
-                    return [[],0]
-                }
-                return [[],0]
-            })(streamData.syncStart, lastSync)
-            let metadata = {
-                channel: channel,
-                streamFile: steamFile,
-                startSync: streamData.startSync - startTime[1],
-                stopSync: streamData.startSync + (maxFileTime * 3600000),
-                targetDuration: (maxFileTime * 3600000),
-                startStream: streamData.startSync,
-                stopStream: streamData.stopSync,
-                droppedSegments: [],
-                closedGracefully: false
-            }
-
-            const recordingFile = `SXM_Digital_${channel}_${streamData.startSync}`;
-
-
-            function writeMetadata() {
-                return new Promise(f => {
-                    fs.writeFile(path.join(config.record_dir, `${recordingFile}.json`), JSON.stringify(metadata).toString(), () => f(null))
-                })
-            }
-
-            console.log(metadata);
-            const spawnedRecorder = spawn('/usr/local/bin/ffmpeg', [
-                '-hide_banner',
-                '-y',
-                '-protocol_whitelist', 'concat,file,http,https,tcp,tls,crypto',
-                '-reconnect', 'true',
-                '-reconnect_on_network_error', 'false',
-                ...startTime[0],
-                '-t', `${maxFileTime}:00:00`,
-                '-i', steamFile,
-                `${recordingFile}.mp3`
-            ], {
-                cwd: config.record_dir,
-                encoding: 'utf8'
-            })
-            activeRecordings.set(channel, spawnedRecorder);
-            writeMetadata();
-
-            spawnedRecorder.stdout.on('data', (data) => {
-                if (data.includes('#EXT-X-PROGRAM-DATE-TIME')) {
-
+function newRadioStream(channelNumber) {
+    return new Promise(resolve => {
+        try {
+            request.get({
+                url: `http://${config.sxmclient_host}/${channelNumber}.m3u8`,
+            }, async function (err, res, body) {
+                if (err) {
+                    console.error("Faulty Server Response: " + err.message);
+                    resolve(false);
                 } else {
-                    console.log(data)
+                    try {
+                        const data = body.toString();
+                        const _data = data.split('\n').map(e => {
+                            if (e.startsWith('#EXT-X-KEY')) {
+                                return e.replace('URI="', `URI="http://${config.sxmclient_host}/`);
+                            } else if (e.startsWith('AAC_Data')) {
+                                return `http://${config.sxmclient_host}/${e}`
+                            } else {
+                                return e
+                            }
+                        })
+                        const startSync = moment(data.find(e => e.startsWith('#EXT-X-PROGRAM-DATE-TIME')).split('PROGRAM-DATE-TIME:').pop().split('+')[0]).valueOf();
+                        const stopSync = moment(data.reverse().find(e => e.startsWith('#EXT-X-PROGRAM-DATE-TIME')).split('PROGRAM-DATE-TIME:').pop().split('+')[0]).valueOf();
+                        resolve({
+                            playlist: _data,
+                            startSync,
+                            stopSync
+                        })
+                    } catch (e) {
+                        console.error('AAC Data Failure');
+                        console.error(e);
+                        resolve(false);
+                    }
                 }
-            });
-            spawnedRecorder.stderr.on('data', (data) => {
-                console.error(data)
-            });
-            spawnedRecorder.on('close', (code) => {
-                rimraf(steamFile);
-                activeRecordings.delete(channel);
-                console.log(`Recorder closed with code: ${code}`);
-                if (code === 0)
-                    metadata.closedGracefully = true;
-                writeMetadata();
-                LastSyncChannels.set(channel, metadata.stopSync);
-                startNewRecording(channel, limiter);
-            });
-        })
-    }
-
-    Object.keys(config.channels).forEach((channelNumber) => {
-        const ch = config.channels[channelNumber]
-        if (ch.id && ch.allowDigital === true) {
-            const limiter = new RateLimiter(5, 60000);
-            startNewRecording(ch.id, limiter);
+            })
+        } catch (e) {
+            console.error(`Failed to get stream URLs!`)
+            console.error(e)
+            resolve(false);
         }
     })
-})()
+}
+function writeStreamSheet(playlist) {
+    return new Promise(resolve => {
+        let token = crypto.randomBytes(64).toString("hex");
+        fs.writeFile(path.join(config.record_dir, `.AACSTREAM_${token}.m3u8`), playlist.toString(), () => resolve(`.AACSTREAM_${token}.m3u8`))
+    })
+}
+function startNewRecording(channel, limiter) {
+    const maxFileTime = 2;
+    limiter.removeTokens(1, async () => {
+        const lastSync = (LastSyncChannels.has(channel)) ? LastSyncChannels.get(channel) : undefined;
+        const streamData = await newRadioStream(channel);
+        const steamFile = await writeStreamSheet(streamData.playlist.join('\n'));
+
+        const startTime = ((x,y) => {
+            if (lastSync) {
+                if (x <= y && (x - y) > 0)
+                    return [[ '-ss', msToTime(x - y) ], y]
+                return [[],0]
+            }
+            return [[],0]
+        })(streamData.syncStart, lastSync)
+        let metadata = {
+            channel: channel,
+            streamFile: steamFile,
+            startSync: streamData.startSync - startTime[1],
+            stopSync: streamData.startSync + (maxFileTime * 3600000),
+            targetDuration: (maxFileTime * 3600000),
+            startStream: streamData.startSync,
+            stopStream: streamData.stopSync,
+            droppedSegments: [],
+            closedGracefully: false
+        }
+
+        const recordingFile = `SXM_Digital_${channel}_${streamData.startSync}`;
+
+
+        function writeMetadata() {
+            return new Promise(f => {
+                fs.writeFile(path.join(config.record_dir, `${recordingFile}.json`), JSON.stringify(metadata).toString(), () => f(null))
+            })
+        }
+
+        console.log(metadata);
+        const spawnedRecorder = spawn('/usr/local/bin/ffmpeg', [
+            '-hide_banner',
+            '-y',
+            '-protocol_whitelist', 'concat,file,http,https,tcp,tls,crypto',
+            '-reconnect', 'true',
+            '-reconnect_on_network_error', 'false',
+            ...startTime[0],
+            '-t', `${maxFileTime}:00:00`,
+            '-i', steamFile,
+            `${recordingFile}.mp3`
+        ], {
+            cwd: config.record_dir,
+            encoding: 'utf8'
+        })
+        activeRecordings.set(channel, spawnedRecorder);
+        writeMetadata();
+
+        spawnedRecorder.stdout.on('data', (data) => {
+            if (data.includes('#EXT-X-PROGRAM-DATE-TIME')) {
+
+            } else {
+                console.log(data)
+            }
+        });
+        spawnedRecorder.stderr.on('data', (data) => {
+            console.error(data)
+        });
+        spawnedRecorder.on('close', (code) => {
+            rimraf(steamFile);
+            activeRecordings.delete(channel);
+            console.log(`Recorder closed with code: ${code}`);
+            if (code === 0)
+                metadata.closedGracefully = true;
+            writeMetadata();
+            LastSyncChannels.set(channel, metadata.stopSync);
+            startNewRecording(channel, limiter);
+        });
+    })
+}
+
+Object.keys(config.channels).forEach((channelNumber) => {
+    const ch = config.channels[channelNumber]
+    if (ch.id && ch.allowDigital === true) {
+        console.log(`Started Channel ${ch.id}`)
+        const limiter = new RateLimiter(5, 60000);
+        startNewRecording(ch.id, limiter);
+    }
+})
