95d9b2c130ceb8819f41a618487e3520297b8dd6
Yukimi Kazari <dev@9854.moe>
2022-03-24 23:38:50 -0400

    Major upgrade that is going to take awhile to debug

--- server.js
+++ server.js
@@ -463,9 +463,10 @@ function listEventsValidated(songs, device, count) {
             return 1
         return 0
     }
-    const events = Object.keys(channelTimes.timetable).filter(e => !device || (device && e === device)).map(d => {
-        return [...channelTimes.timetable[d].map((tc, i, a) => {
-            return [...metadata[tc.ch].filter(f =>
+    let events = []
+   Object.keys(channelTimes.timetable).filter(e => !device || (device && e === device)).map(d => {
+        return channelTimes.timetable[d].map((tc, i, a) => {
+            return metadata[tc.ch].filter(f =>
                 // Has duration aka is completed
                 parseInt(f.duration.toString()) > 90 &&
                 // First Item or Was Tuned after event start
@@ -477,14 +478,15 @@ function listEventsValidated(songs, device, count) {
             ).map((f, i, a) => {
                 if ((!f.duration || f.duration === 0) && (i !== a.length - 1) && (a[i + 1].syncStart))
                     f.syncEnd = a[i + 1].syncStart - 1
-                return {
+                events.push({
                     ...f,
                     channelId: tc.ch,
                     tunerId: d
-                }
-            })]
-        })]
-    }).sort(sortEvents)
+                })
+            })
+        })
+    })
+    events.sort(sortEvents)
     console.log(events)
     if (count)
         return (events.length > count) ? events.slice(Math.abs(count) * -1) : events
