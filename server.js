a0120137e08a83e9c123d6c0c85ddf1040eac00e
Yukimi Kazari <dev@9854.moe>
2022-03-25 01:36:46 -0400

    Major upgrade that is going to take awhile to debug

--- server.js
+++ server.js
@@ -1351,7 +1351,7 @@ app.get("/debug/digital/:tuner", async (req, res, next) => {
             let chid = false
             if (req.query.ch) {
                 chid = getChannelbyNumber(req.query.ch)
-                const tune = await tuneDigitalChannel(chid, (moment().subtract(15, "minutes").valueOf() + ((t.delay) ? t.delay * 1000 : 0)), t.serial)
+                const tune = await tuneDigitalChannel(chid.id, (moment().subtract(15, "minutes").valueOf() + ((t.delay) ? t.delay * 1000 : 0)), t.serial)
                 if (tune) {
                     const record = await recordAudioInterface(t, "00:01:00", `Extracted_test`)
                     if (record) {
