e3457f2e2029b7066acc3baa26817b58400bb782
Yukimi Kazari <dev@9854.moe>
2022-03-25 01:03:07 -0400

    Major upgrade that is going to take awhile to debug

--- server.js
+++ server.js
@@ -1310,7 +1310,7 @@ app.get("/pend_bounce", (req, res) => {
     } else if (req.query.ch) {
         const chid = getChannelbyNumber(req.query.ch)
         if (chid) {
-            registerBounce((req.query.add_time) ? parseInt(req.query.add_time) : 0, chid, undefined, (req.query.digitalOnly && req.query.digitalOnly === "true") ? true : undefined);
+            registerBounce((req.query.add_time) ? parseInt(req.query.add_time) : 0, chid.id, undefined, (req.query.digitalOnly && req.query.digitalOnly === "true") ? true : undefined);
             res.status(200).send('OK')
         } else {
             req.status(404).send('Unknown Channel')
