7f5968a7066accefa5565282a2df80028e82af34
Yukimi Kazari <dev@9854.moe>
2022-03-25 02:03:31 -0400

    Major upgrade that is going to take awhile to debug

--- server.js
+++ server.js
@@ -1129,6 +1129,8 @@ function recordAudioInterface(tuner, time, name) {
 }
 // Tune, Record, Disconnect
 async function recordDigitalEvent(eventItem, tuner) {
+    console.log(eventItem)
+    console.log(tuner)
     if (await tuneDigitalChannel(eventItem.channelId, (eventItem.syncStart + ((tuner.delay) ? tuner.delay * 1000 : (eventItem.delay) ? eventItem.delay * 1000 : 0)), tuner.serial)) {
         const recordedEvent = await recordAudioInterface(tuner, msToTime((parseInt(eventItem.duration.toString()) * 1000) + 30000), `Extracted_${eventItem.guid}`)
         if (tuner.record_only)
