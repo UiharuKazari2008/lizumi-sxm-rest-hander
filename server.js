c37ee0890693a52c1868ed2f8fcf48c563899bf0
Yukimi Kazari <dev@9854.moe>
2022-03-26 05:05:08 -0400

    Beta Update adding new recording and magement system

--- server.js
+++ server.js
@@ -1384,7 +1384,7 @@ console.log("Settings up recorder queues...")
 for (let t of listTuners()) {
     const mq = new Queue(`${(t.digital) ? 'D-': 'A-'}${t.id}`)
     if (t.digital) {
-        const socketready = await startAudioDevice(t);
+        const socketready = (async () => { return await startAudioDevice(t) })();
         if (socketready) {
             mq.process(async (job, done) => {
                 console.log(`Processing Job for Tuner ${t.id} ${job.id}`);
