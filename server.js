7176ad5b1b64f37c86455c3053f797e1f37ef5e3
Yukimi Kazari <dev@9854.moe>
2022-03-28 01:30:21 -0400

    Updates for digital recorder

--- server.js
+++ server.js
@@ -844,7 +844,7 @@ function registerSchedule() {
 }
 //
 function searchEvents() {
-    const events = listEventsValidated(false, undefined, 25)
+    const events = listEventsValidated(false, undefined, 50)
     Object.values(config.autosearch_terms).map(f => {
         events.filter(e => channelTimes.completed.indexOf(e.guid) === -1 && e.filename && e.filename.toLowerCase().includes(f.search.toLowerCase()) && e.duration > 900 && (!f.duration || (f.duration && e.duration > f.duration))).map(e => {
             console.log(`Found Event ${e.filename} - ${e.duration}`)
