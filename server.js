cef5390e023cebdd9e1f7eba739174de6cbcd7de
Yukimi Kazari <dev@9854.moe>
2022-03-28 01:38:18 -0400

    Updates for digital recorder

--- server.js
+++ server.js
@@ -730,7 +730,7 @@ async function processPendingBounces() {
                 pendingEvent.inprogress = false
             } else {
                 // If Event has completed
-                if (thisEvent.duration && parseInt(thisEvent.duration.toString()) > 0 && thisEvent.syncStart <= moment().valueOf() + 5 * 60000 && (pendingEvent.restrict && isWantedEvent(pendingEvent.restrict, thisEvent))) {
+                if (thisEvent.duration && parseInt(thisEvent.duration.toString()) > 0 && thisEvent.syncStart <= moment().valueOf() + 5 * 60000 && (!pendingEvent.restrict || (pendingEvent.restrict && isWantedEvent(pendingEvent.restrict, thisEvent)))) {
                     if (!pendingEvent.failedRec && (moment.utc(thisEvent.syncStart).local().valueOf() >= (Date.now() - 14400000)) && !pendingEvent.tuner && digitalAvailable && !config.disable_digital_extraction) {
                         // If not failed event, less then 3 hours old, not directed to a specifc tuner, digital recorder ready, and enabled
                         pendingEvent.guid = thisEvent.guid;
@@ -760,7 +760,7 @@ async function processPendingBounces() {
                             index: true
                         })
                     }
-                } else if (Math.abs(Date.now() - parseInt(thisEvent.syncStart.toString())) >= ((thisEvent.delay) + (5 * 60) * 1000) && (pendingEvent.digitalOnly || config.live_extract) && (pendingEvent.restrict && isWantedEvent(pendingEvent.restrict, thisEvent))) {
+                } else if (Math.abs(Date.now() - parseInt(thisEvent.syncStart.toString())) >= ((thisEvent.delay) + (5 * 60) * 1000) && (pendingEvent.digitalOnly || config.live_extract) && (!pendingEvent.restrict || (pendingEvent.restrict && isWantedEvent(pendingEvent.restrict, thisEvent)))) {
                     // Event is 5 min past its start (accounting for digital delay), digital only event or live extract is enabled
                     pendingEvent.guid = thisEvent.guid;
                     pendingEvent.liveRec = true
@@ -844,9 +844,9 @@ function registerSchedule() {
 }
 //
 function searchEvents() {
-    const events = listEventsValidated(false, undefined, 100)
+    const events = listEventsValidated(false, undefined, 50)
     Object.values(config.autosearch_terms).map(f => {
-        events.filter(e => channelTimes.completed.indexOf(e.guid) === -1 && e.filename && e.filename.toLowerCase().includes(f.search.toLowerCase())).map(e => {
+        events.filter(e => channelTimes.completed.indexOf(e.guid) === -1 && e.filename && e.filename.toLowerCase().includes(f.search.toLowerCase()) && e.duration > 900).map(e => {
             console.log(`Found Event ${e.filename} ${e.guid} - ${e.duration}`)
             channelTimes.completed.push(e.guid)
             channelTimes.pending.push({
