1af75dd6f6d403ad05c066ca4fc7b302278a97da
Yukimi Kazari <dev@9854.moe>
2022-04-07 15:26:15 -0400

    Moved Filter for events earlier to lesen the proccessing time

--- server.js
+++ server.js
@@ -2450,8 +2450,11 @@ app.use("/debug", (req, res) => {
 app.get("/status/:type", async (req, res) => {
     try {
         switch (req.params.type) {
+            case 'songs':
+                res.status(200).json(formatEventList(listEventsValidated(true, undefined, 1000)))
+                break;
             case 'events':
-                res.status(200).json(formatEventList(listEventsValidated(undefined, undefined)))
+                res.status(200).json(formatEventList(listEventsValidated(false, undefined, 1000)))
                 break;
             case 'devices':
                 const source = await getAirOutput()
