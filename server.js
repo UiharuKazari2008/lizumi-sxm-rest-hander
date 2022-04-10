f570793a2d42d9e5c80c01174a86171fd7c02875
Yukimi Kazari <dev@9854.moe>
2022-04-10 13:29:32 -0400

    Corrects error with 0 duration events when the events is way to short

--- server.js
+++ server.js
@@ -751,9 +751,10 @@ function cacheEventsValidated() {
                                 if (guidMap.indexOf(f.guid) === -1) {
                                     if ((!f.duration || f.duration === 0) && (i !== a.length - 1) && a[i + 1].syncStart) {
                                         f.syncEnd = a[i + 1].syncStart - 1
-                                        f.duration = ((f.syncEnd - f.syncStart) / 1000).toFixed(0)
-                                        if (f.duration === 0)
+                                        f.duration = parseInt(((f.syncEnd - f.syncStart) / 1000).toFixed(0))
+                                        if (f.duration === 0) {
                                             f.duration = 1
+                                        }
                                     }
                                     if (!f.filename) {
                                         f.filename = (() => {
