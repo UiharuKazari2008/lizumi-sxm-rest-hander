7a5af2c1d3dc3c27c4223f0c3b2b6750ad80de3a
Yukimi Kazari <dev@9854.moe>
2022-04-14 18:49:45 -0400

    End meta items when there are <1 sec bummpers

--- server.js
+++ server.js
@@ -874,7 +874,7 @@
                         } else if ((!f.duration || f.duration === 0 || f.duration === "0") && (i !== a.length - 1) && (a[i + 1].syncStart && !a[i + 1].isEpisode)) {
                             f.syncEnd = a[i + 1].syncStart - 1
                             f.duration = parseInt(((f.syncEnd - f.syncStart) / 1000).toFixed(0))
-                        } else if ((!f.duration || f.duration === 0 || f.duration === "0") && (i !== a.length - 1) && f.syncEnd - f.syncStart > 2) {
+                        } else if ((!f.duration || f.duration === 0 || f.duration === "0") && (i !== a.length - 1) && Math.abs(f.syncEnd - f.syncStart) > 2) {
                             f.duration = 1
                         } else if (i === a.length - 1 && !isActive && (!channelConfig || (channelConfig && !channelConfig.updateOnTune))) {
                             f.duration = 1
