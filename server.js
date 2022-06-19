f94500dbd0632863ee7785f1ee4ce84542cb2a74
Yukimi Kazari <dev@9854.moe>
2022-06-18 22:14:49 -0400

    Added playback of events

--- server.js
+++ server.js
@@ -2709,7 +2709,7 @@
                     const tuners = listTuners().map(e => {
                         const meta = (e.activeCh && !e.activeCh.hasOwnProperty("end")) ? nowPlaying(e.activeCh.ch) : false
                         const activeJob = activeJobs.filter(j => j.queue.slice(4) === e.id)
-                        const channelMeta = (activeJob.length > 0) ? activeJob.map(j => getEvent(undefined, j.guid))[0] : (watchdog_tuners[e.id].player_guid) ? getEvent(undefined, watchdog_tuners[e.id].player_guid) : (meta) ? meta : false
+                        const channelMeta = (activeJob.length > 0) ? activeJob.map(j => getEvent(undefined, j.guid))[0] : (e.digital && watchdog_tuners[e.id].player_guid) ? getEvent(undefined, watchdog_tuners[e.id].player_guid) : (meta) ? meta : false
                         return {
                             id: e.id,
                             name: e.name,
